be5793134fa90cae91fc366041180ecd14bf4aa2
JuanXavier <juanxaviervm@hotmail.com>
2022-09-13 13:07:52 -0500

    setup (#1)

--- src/Attacker.sol
+++ src/Attacker.sol
@@ -2,32 +2,31 @@
 pragma solidity ^0.8.15;
 
 import "forge-std/console.sol";
-import {ERC20} from "./ERC20.sol";
-import {IUniswapV2Pair} from "./IUniswapV2.sol";
-import {LendingProtocol} from "./LendingProtocol.sol";
-
+import { ERC20 } from "./ERC20.sol";
+import { IUniswapV2Pair } from "./IUniswapV2.sol";
+import { LendingProtocol } from "./LendingProtocol.sol";
 
 /// @title Attacker
 /// @author Christoph Michel <cmichel.io>
 contract Attacker {
-    IUniswapV2Pair public immutable pair; // token0 <> token1 uniswapv2 pair
-    ERC20 public immutable ctf; // token0
-    ERC20 public immutable usd; // token1
-    LendingProtocol public immutable lending;
+	IUniswapV2Pair public immutable pair; // token0 <> token1 uniswapv2 pair
+	ERC20 public immutable ctf; // token0
+	ERC20 public immutable usd; // token1
+	LendingProtocol public immutable lending;
 
-    constructor(
-        ERC20 _ctf,
-        ERC20 _usd,
-        IUniswapV2Pair _pair,
-        LendingProtocol _lending
-    ) {
-        ctf = _ctf;
-        usd = _usd;
-        pair = _pair;
-        lending = _lending;
-    }
+	constructor(
+		ERC20 _ctf,
+		ERC20 _usd,
+		IUniswapV2Pair _pair,
+		LendingProtocol _lending
+	) {
+		ctf = _ctf;
+		usd = _usd;
+		pair = _pair;
+		lending = _lending;
+	}
 
-    function attack() external {
-        console.log("implement attack here");
-    }
+	function attack() external {
+		console.log("implement attack here");
+	}
 }
